846bcaa27b89993815f2c75fa70b88e9631c5fb8
zzo38 <108554049+zzo38@users.noreply.github.com>
2025-06-09 23:25:18 +0000

    Implement the other wave sound codecs. (Creative 3-bits ADPCM is not tested)

--- audio.c
+++ audio.c
@@ -157,19 +157,64 @@ static inline Uint8 adpcm4bits(Uint8 v,Uint8 p,Uint8*m) {
   return p=(r<0?0:r>255?255:r);
 }
 
-#if 0
 static inline Uint8 adpcm3bits(Uint8 v,Uint8 p,Uint8*m) {
-  //TODO
+  // This is not tested yet.
+  static const Sint8 u[40]={
+    0,1,2,3,-0,-1,-2,-3,
+    1,3,5,7,-1,-3,-5,-7,
+    2,6,10,14,-2,-6,-10,-14,
+    4,12,20,28,-4,-12,-20,-28,
+    5,15,25,35,-5,-15,-25,-35,
+  };
+  Sint32 r=p+u[v+*m];
+  if((v&4)==3 && *m<32) *m+=8; else if((v&4)==0 && *m>4) *m-=8;
+  return p=(r<0?0:r>255?255:r);
 }
-#endif
 
 static inline Uint8 adpcm2bits(Uint8 v,Uint8 p,Uint8*m) {
   static const Sint8 u[24]={0,1,-0,-1,1,3,-1,-3,2,6,-2,-6,4,12,-4,-12,8,24,-8,-24,16,48,-16,-48};
-  Sint32 r;
-  r=p+u[v+*m];
+  Sint32 r=p+u[v+*m];
   *m+=(v&1?(*m<20?4:0):(*m<4?0:-4));
   return p=(r<0?0:r>255?255:r);
 }
+
+static const Sint16 alaw[128]={
+  5504, 5248, 6016, 5760, 4480, 4224, 4992, 4736,
+  7552, 7296, 8064, 7808, 6528, 6272, 7040, 6784,
+  2752, 2624, 3008, 2880, 2240, 2112, 2496, 2368,
+  3776, 3648, 4032, 3904, 3264, 3136, 3520, 3392,
+  22016, 20992, 24064, 23040, 17920, 16896, 19968, 18944,
+  30208, 29184, 32256, 31232, 26112, 25088, 28160, 27136,
+  11008, 10496, 12032, 11520, 8960, 8448, 9984, 9472,
+  15104, 14592, 16128, 15616, 13056, 12544, 14080, 13568,
+  344, 328, 376, 360, 280, 264, 312, 296,
+  472, 456, 504, 488, 408, 392, 440, 424,
+  88, 72, 120, 104, 24, 8, 56, 40,
+  216, 200, 248, 232, 152, 136, 184, 168,
+  1376, 1312, 1504, 1440, 1120, 1056, 1248, 1184,
+  1888, 1824, 2016, 1952, 1632, 1568, 1760, 1696,
+  688, 656, 752, 720, 560, 528, 624, 592,
+  944, 912, 1008, 976, 816, 784, 880, 848,
+};
+
+static const Sint16 mulaw[128]={
+   32124, 31100, 30076, 29052, 28028, 27004, 25980, 24956,
+   23932, 22908, 21884, 20860, 19836, 18812, 17788, 16764,
+   15996, 15484, 14972, 14460, 13948, 13436, 12924, 12412,
+   11900, 11388, 10876, 10364,  9852,  9340,  8828,  8316,
+    7932,  7676,  7420,  7164,  6908,  6652,  6396,  6140,
+    5884,  5628,  5372,  5116,  4860,  4604,  4348,  4092,
+    3900,  3772,  3644,  3516,  3388,  3260,  3132,  3004,
+    2876,  2748,  2620,  2492,  2364,  2236,  2108,  1980,
+    1884,  1820,  1756,  1692,  1628,  1564,  1500,  1436,
+    1372,  1308,  1244,  1180,  1116,  1052,   988,   924,
+     876,   844,   812,   780,   748,   716,   684,   652,
+     620,   588,   556,   524,   492,   460,   428,   396,
+     372,   356,   340,   324,   308,   292,   276,   260,
+     244,   228,   212,   196,   180,   164,   148,   132,
+     120,   112,   104,    96,    88,    80,    72,    64,
+      56,    48,    40,    32,    24,    16,     8,     0,
+};
 
 static int convertwave(FILE*f,Uint32 size,WaveSound*wav,Uint16 rate,Uint8 flag1,Uint8 flag2) {
   // For the ADPCM formats, also see the files:
@@ -198,7 +243,6 @@ static int convertwave(FILE*f,Uint32 size,WaveSound*wav,Uint16 rate,Uint8 flag1,
       }
       if(convertaudio(wav,rate,flag1&1,AUDIO_U8)) return 1;
       break;
-#if 0
     case 2: // ADPCM 3-bits
       wav->data=(void*)(d=calloc((wav->len=size*3)+1,2));
       if(!wav->data) err(1,"Memory error in wave sound conversion");
@@ -211,7 +255,6 @@ static int convertwave(FILE*f,Uint32 size,WaveSound*wav,Uint16 rate,Uint8 flag1,
       }
       if(convertaudio(wav,rate,flag1&1,AUDIO_U8)) return 1;
       break;
-#endif
     case 3: // ADPCM 2-bits
       wav->data=(void*)(d=calloc((wav->len=size*4)+1,2));
       if(!wav->data) err(1,"Memory error in wave sound conversion");
@@ -231,21 +274,19 @@ static int convertwave(FILE*f,Uint32 size,WaveSound*wav,Uint16 rate,Uint8 flag1,
       fread(wav->data,2,wav->len,f);
       if(rate!=spec.freq || !(flag1&1) || AUDIO_S16SYS!=AUDIO_S16LSB) if(convertaudio(wav,rate,flag1&1,AUDIO_S16LSB)) return 1;
       break;
-#if 0
     case 6: // A-law
       wav->data=calloc(wav->len=size,2);
       if(!wav->data) err(1,"Memory error in wave sound conversion");
-      for(a=0;a<wav->len;a++) wav->data[a]=alaw[fgetc(f)&255];
+      for(a=0;a<wav->len;a++) p=fgetc(f),wav->data[a]=p&128?-alaw[p&127]:alaw[p&127];
       if(rate!=spec.freq || !(flag1&1)) if(convertaudio(wav,rate,flag1&1,AUDIO_S16SYS)) return 1;
       break;
     case 7: // mu-law
       wav->data=calloc(wav->len=size,2);
       if(!wav->data) err(1,"Memory error in wave sound conversion");
-      for(a=0;a<wav->len;a++) wav->data[a]=mulaw[fgetc(f)&255];
+      for(a=0;a<wav->len;a++) p=fgetc(f),wav->data[a]=p&128?mulaw[p&127]:(-mulaw[p&127]-(p==127));
       if(rate!=spec.freq || !(flag1&1)) if(convertaudio(wav,rate,flag1&1,AUDIO_S16SYS)) return 1;
       break;
-#endif
-    default: fprintf(stderr,"Unknown codec %d\n",flag2&15); return 1;
+    default: fprintf(stderr,"Unknown audio codec %d\n",flag2&15); return 1;
   }
   if(flag1&2) {
     // Apply filter
